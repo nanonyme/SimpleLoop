c923d9eaf57ebf7978b2ba91b8ee598bdbe3d2d6
Seppo Yli-Olli <seppo.yliolli@gmail.com>
2011-03-01 23:54:44 +0200

    Fix updatedocs.sh

--- updatedocs.sh
+++ updatedocs.sh
@@ -5,7 +5,7 @@ fi
 cd docs
 if [ ! -d .git ];then
 git init
-git remote add git://github.com/nanonyme/SimpleLoop.wiki.git
+git remote add origin git://github.com/nanonyme/SimpleLoop.wiki.git
 fi
 git pull origin master
 cd ..
