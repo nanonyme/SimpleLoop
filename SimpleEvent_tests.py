907bf941239a5f69efa9f2c6b276690ff1194dda
Seppo Yli-Olli <seppo.yliolli@gmail.com>
2011-03-03 03:56:08 +0200

    Some beginnings of unit tests

---
+++ SimpleEvent_tests.py
@@ -0,0 +1,33 @@
+import unittest
+from SimpleEvent import *
+
+# 
+def run_test(function, args):
+    try:
+        function(args)
+    except AssertionError:
+        return False
+    return True
+
+class EventFactoryTests(unittest.TestCase):
+    def setUp(self):
+        self.factory = EventFactory()
+
+    def tearDown(self):
+        self.factory = None
+
+    def test_negativeMaxCount(self):
+        success = run_test(lambda (x, y): x.setMaxCount(y),
+                           (self.factory, -5))
+        self.assertEqual(success, False,
+                         "Should have refused negative value")
+
+
+    def test_recycleNone(self):
+        success = run_test(lambda (x, y): x.recycleEvent(y),
+                           (self.factory, None))
+        self.assertEqual(success, False,
+                         "Should have refused recycling None object")
+
+if __name__ == '__main__':
+        unittest.main()
---
+++ runtests.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+PY_DBC=1
+export PY_DBC
+python SimpleEvent_tests.py
